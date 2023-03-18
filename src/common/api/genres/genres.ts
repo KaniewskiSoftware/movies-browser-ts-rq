03c40ed541a042e2f3212526ada6b72673603fcb
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-18 16:47:04 +0100

    genres conversion to object added for usage ease

--- src/common/api/genres/genres.ts
+++ src/common/api/genres/genres.ts
@@ -2,13 +2,35 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { apiLink } from "../parameters/apiLink";
 import { apiKey } from "../parameters/apiKey";
-import { GenresResponse } from "../../apiResponseTypes/genres/genres";
+import { Genre, GenresResponse } from "../../apiResponseTypes/genres/genres";
+
+type GenresObject = {
+  [id: number]: string;
+};
 
 const fetchGenres = async (): Promise<GenresResponse> => {
   const response = await axios.get(`${apiLink}/genre/movie/list${apiKey}`);
   return response.data;
 };
 
+const convertGenresArrayToObject = (genres: Genre[]): GenresObject =>
+  genres.reduce<GenresObject>((accumulator, { id, name }) => {
+    if (id !== undefined && name !== undefined) {
+      return {
+        ...accumulator,
+        [id]: name,
+      };
+    }
+
+    return accumulator;
+  }, {});
+
 export const useGenres = () => {
-  return useQuery(["genres"], fetchGenres);
+  const query = useQuery(["genres"], fetchGenres);
+
+  const genresObject = query.data?.genres
+    ? convertGenresArrayToObject(query.data.genres)
+    : {};
+
+  return { ...query, data: { ...query.data, genres: genresObject } };
 };
