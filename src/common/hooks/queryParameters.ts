fc4927dc37c2735b6b2daa6c78a5438fe1805e70
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-20 15:15:40 +0100

    return type declared using ReturnType.

--- src/common/hooks/queryParameters.ts
+++ src/common/hooks/queryParameters.ts
@@ -1,34 +1,32 @@
 import { useLocation, useSearchParams } from "react-router-dom";
 
-type UseQueryParameterFunction = (key: string) => number | string | null; // Returns number while used with Pagination
-
 interface ReplaceQueryParameterProps {
   key: string;
   value: string | null;
 }
 
-type UseReplaceQueryParameterFunction = () => (
-  props: ReplaceQueryParameterProps
-) => void; //returns function, the returned function gets props with the ReplaceQueryParameterProps type and returns void.
-
-export const useQueryParameter: UseQueryParameterFunction = (key) => {
+export const useQueryParameter = (key: string) => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   return searchParams.get(key);
 };
 
-export const useReplaceQueryParameter: UseReplaceQueryParameterFunction =
-  () => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    return ({ key, value }: ReplaceQueryParameterProps) => {
-      if (!value) {
-        searchParams.delete(key);
-      } else {
-        searchParams.set(key, value);
-      }
-      setSearchParams(searchParams);
-    };
+export const useReplaceQueryParameter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  return ({ key, value }: ReplaceQueryParameterProps) => {
+    if (!value) {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    }
+    setSearchParams(searchParams);
   };
+};
 
 export const searchQueryParamName = "search";
 export const pageQueryParamName = "page";
+
+export type UseQueryParameterType = ReturnType<typeof useQueryParameter>;
+export type UseReplaceQueryParameter = ReturnType<
+  typeof useReplaceQueryParameter
+>;
