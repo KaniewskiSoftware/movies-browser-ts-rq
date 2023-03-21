a40494e9c4f159b78e2c14efa101ab863613eb23
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-21 19:38:32 +0100

    serach logic moved to custom hook

--- src/common/Header/Search/index.tsx
+++ src/common/Header/Search/index.tsx
@@ -1,47 +1,13 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import { createSearchParams, useLocation, useNavigate } from "react-router-dom";
-import {
-  searchQueryParamName,
-} from "../../hooks/queryParameters";
-import { debounce } from "../../debounce";
-import { toMovie, toMovies, toPeople, toPerson } from "../../routes";
+import { useMemo } from "react";
+import { useLocation } from "react-router-dom";
 import svg from "./Search.svg";
 import { Input, InputWrapper, Loupe } from "./styled";
+import useSearchLogic from "./useSearchLogic";
 
 const Search = () => {
   const location = useLocation();
-  const navigate = useNavigate();
-  const [inputValue, setInputValue] = useState("");
 
-  useEffect(() => {
-    if (!location.search) setInputValue("");
-  }, [location.search]);
-
-  const debouncedNavigate = useRef(
-    debounce((value: string, currentPath: string) => {
-      if (currentPath.split("/")[1] === toMovie.split("/")[1]) {
-        navigate({
-          pathname: toMovies,
-          search: createSearchParams({
-            [searchQueryParamName]: value,
-          }).toString(),
-        });
-      } else if (currentPath.split("/")[1] === toPerson.split("/")[1]) {
-        navigate({
-          pathname: toPeople,
-          search: createSearchParams({
-            [searchQueryParamName]: value,
-          }).toString(),
-        });
-      }
-    }, 500)
-  );
-
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setInputValue(value);
-    debouncedNavigate.current(value, location.pathname);
-  };
+  const { inputValue, onInputChange } = useSearchLogic();
 
   const placeholderText = useMemo(() => {
     return location.pathname.includes("people")
---
+++ src/common/Header/Search/useSearchLogic.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import { createSearchParams, useLocation, useNavigate } from "react-router-dom";
+import { searchQueryParamName } from "../../hooks/queryParameters";
+import { debounce } from "../../debounce";
+import { toMovie, toMovies, toPeople, toPerson } from "../../routes";
+
+const useSearchLogic = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [inputValue, setInputValue] = useState("");
+  
+    useEffect(() => {
+      if (!location.search) setInputValue("");
+    }, [location.search]);
+  
+    const debouncedNavigate = useRef(
+      debounce((value: string, currentPath: string) => {
+        if (currentPath.split("/")[1] === toMovie.split("/")[1]) {
+          navigate({
+            pathname: toMovies,
+            search: createSearchParams({
+              [searchQueryParamName]: value,
+            }).toString(),
+          });
+        } else if (currentPath.split("/")[1] === toPerson.split("/")[1]) {
+          navigate({
+            pathname: toPeople,
+            search: createSearchParams({
+              [searchQueryParamName]: value,
+            }).toString(),
+          });
+        }
+      }, 500)
+    );
+  
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      setInputValue(value);
+      debouncedNavigate.current(value, location.pathname);
+    };
+  
+    return { inputValue, setInputValue, onInputChange };
+  };
+  
+  export default useSearchLogic;
