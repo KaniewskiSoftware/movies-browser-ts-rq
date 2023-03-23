e769119d573ad75eb7036732c701f717789ddee1
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-23 22:21:02 +0100

    description provided

--- src/common/utils/buildImageURL.ts
+++ src/common/utils/buildImageURL.ts
@@ -1,5 +1,5 @@
 export const buildImageURL = (
-  size: "w92" | "w154" | "w185" | "w342" | "w500" | "w780" | "original",
+  size: "w92" | "w154" | "w185" | "w342" | "w500" | "w780" | "original", // Possible poster sizes from the API.
   imagePath: string | null | undefined,
 ): string | null => {
   if (!imagePath) return null;
