9935a97a021b85b34e5b4792cf74726054e0ab64
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-23 15:06:04 +0100

    grid for people added

--- src/common/components/TilesGrid/index.ts
+++ src/common/components/TilesGrid/index.ts
@@ -1,6 +1,10 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const TilesGrid = styled.div`
+interface TilesGridProps {
+  $moreItems?: boolean;
+}
+
+export const TilesGrid = styled.div<TilesGridProps>`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 24px;
@@ -18,4 +22,27 @@ export const TilesGrid = styled.div`
     grid-template-columns: 1fr;
     grid-auto-rows: 1fr;
   }
+
+  ${({ $moreItems }) =>
+    $moreItems &&
+    css`
+      grid-template-columns: repeat(5, 1fr);
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+        grid-template-columns: repeat(4, 1fr);
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 16px;
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+        grid-template-columns: repeat(2, minmax(136px, 1fr));
+      }
+    `}
 `;
