3b3addb3da8cb99863db41bb454d6b9fa24f8848
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-25 18:36:22 +0100

    documentation provided

--- src/features/movies/Movies/Content/index.tsx
+++ src/features/movies/Movies/Content/index.tsx
@@ -17,12 +17,23 @@ interface MoviesPageContentProps {
   totalResults: number;
 }
 
+/**
+ * Content component displays a list of movie tiles based on the provided data.
+ * It uses the TilesGrid, Tile, Title, and Wrapper components.
+ */
 const Content = ({
   genres,
   movies,
   query,
   totalResults,
 }: MoviesPageContentProps) => {
+  /**
+   * Determines the appropriate poster size for the current screen resolution using
+   * the useResponsiveImageSize custom hook and casts the result to PosterSize.
+   * The type casting is necessary because TypeScript sometimes has difficulty
+   * recognizing that the hook always returns the correct size type for all possible
+   * image type keys, even though the hook ensures the proper type is returned.
+   */
   const size = useResponsiveImageSize("poster") as PosterSize;
   return (
     <Wrapper>
--- src/features/people/People/Content/index.tsx
+++ src/features/people/People/Content/index.tsx
@@ -14,11 +14,22 @@ interface PopularMoviesPageContentProps {
   totalResults: number;
 }
 
+/**
+ * Content component displays a list of movie tiles based on the provided data.
+ * It uses the TilesGrid, Tile, Title, and Wrapper components.
+ */
 const Content = ({
   people,
   query,
   totalResults,
 }: PopularMoviesPageContentProps) => {
+  /**
+   * Determines the appropriate poster size for the current screen resolution using
+   * the useResponsiveImageSize custom hook and casts the result to PosterSize.
+   * The type casting is necessary because TypeScript sometimes has difficulty
+   * recognizing that the hook always returns the correct size type for all possible
+   * image type keys, even though the hook ensures the proper type is returned.
+   */
   const size = useResponsiveImageSize("profile") as ProfileSize;
   return (
     <Wrapper>
