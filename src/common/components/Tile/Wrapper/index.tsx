d2bf97f745978c232496a20d3b08cc2166f1131f
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-25 17:30:31 +0100

    documentation provided

--- src/common/components/Tile/Wrapper/index.tsx
+++ src/common/components/Tile/Wrapper/index.tsx
@@ -5,6 +5,14 @@ interface WrapperProps {
   children: React.ReactNode;
 }
 
+/**
+ * The Wrapper component is used to conditionally wrap its children with a TileLink.
+ * If the 'linkTo' prop is provided, the children will be wrapped with a TileLink.
+ * Otherwise, the children will be rendered as-is.
+ *
+ * @param linkTo - (Optional) A string representing the URL to navigate to when the TileLink is clicked.
+ * @param children - The React child elements to be rendered within the Wrapper component.
+ */
 const Wrapper = ({ linkTo, children }: WrapperProps) => {
   return linkTo ? <TileLink to={linkTo}>{children}</TileLink> : <>{children}</>;
 };
