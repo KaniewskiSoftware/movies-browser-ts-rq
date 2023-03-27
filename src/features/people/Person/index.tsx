97f002c5058061c627b02916cf7febf3a4539272
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-27 11:07:55 +0200

    documentation fix

--- src/features/people/Person/index.tsx
+++ src/features/people/Person/index.tsx
@@ -6,9 +6,9 @@ import Page from "../../../common/components/Page";
 import Content from "./Content";
 
 /**
- * The MovieDetailsPage component fetches and displays detailed information
- * about a single movie. It uses the useMovieDetails custom hook to fetch
- * data about a movie with a given ID from the API, and renders the Page and
+ * The PersonDetailsPage component fetches and displays detailed information
+ * about a single person. It uses the usePersonDetails custom hook to fetch
+ * data about a person with a given ID from the API, and renders the Page and
  * Content components with the fetched data.
  *
  * @returns A React component that renders detailed information about a movie.
