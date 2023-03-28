1da8a388b548b2bd95180fcd51f1479eab0d99c1
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-28 16:48:11 +0200

    documentation update

--- src/common/components/Text/index.ts
+++ src/common/components/Text/index.ts
@@ -7,6 +7,24 @@ interface TextProps {
   $displayOnMobile?: boolean;
   $hidden?: boolean;
 }
+
+/**
+ * textStyles is a set of reusable CSS properties for styled components that
+ * require text-related styles. It includes common text styling properties
+ * and responsive display properties based on the provided TextProps.
+ *
+ * @param $displayOnDesktop - (Optional) A boolean that hides the component on desktop devices
+ *                            (below the 'medium' breakpoint) when true.
+ * @param $displayOnMobile - (Optional) A boolean that hides the component on mobile devices
+ *                           (above the 'medium' breakpoint) when true.
+ * @param $hidden - (Optional) A boolean that hides the component when true.
+ *
+ * @example
+ * // Usage in a styled component
+ * const CustomText = styled.p`
+ *   ${textStyles};
+ * `;
+ */
 export const textStyles = css<TextProps>`
   margin: 0;
   font-weight: 400;
@@ -50,6 +68,8 @@ export const textStyles = css<TextProps>`
  * @param $displayOnMobile - (Optional) A boolean that hides the component on mobile devices
  *                           (above the 'large' breakpoint) when true.
  * @param $hidden - (Optional) A boolean that hides the component when true.
+ * @param textStyles - A set of reusable styles that handle
+ *                     display properties.
  */
 export const Text = styled.p<TextProps>`
   font-size: 18px;
@@ -93,6 +113,14 @@ export const Text = styled.p<TextProps>`
     ${textStyles}
 `;
 
+/**
+ * The Description component is a styled paragraph component that is designed
+ * to display a text description with a larger font size than the Text component.
+ * It inherits the textStyles from to maintain consistent behavior.
+ *
+ * @param textStyles - A set of reusable styles inherited from the Text component that
+ *                     handle display properties based on breakpoints and visibility.
+ */
 export const Description = styled.p`
   font-size: 20px;
   line-height: 1.6;
--- src/common/components/Tile/Link/styled.ts
+++ src/common/components/Tile/Link/styled.ts
@@ -4,6 +4,7 @@ import styled from "styled-components";
 /**
  * TileLink is a styled component based on the Link component from react-router-dom.
  * It is used as a container for the Tile components, providing navigation between pages.
+ * When hovered, it updates the border color of its first child element to the theme's primary color.
  *
  * @example
  * <TileLink to="/movie/12345">
@@ -14,7 +15,7 @@ export const TileLink = styled(Link)`
   text-decoration: none;
   transition: border 1s;
 
-  &:hover > * {
+  &:hover > :first-child {
     border-color: ${({ theme }) => theme.colors.primary};
   }
 `;
