ce32640255588f68bf7208e7753a199ec9572dda
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-03-31 22:21:19 +0200

    names fix

--- src/common/components/Tile/Link/index.tsx
+++ src/common/components/Tile/Link/index.tsx
@@ -1,19 +1,19 @@
 import { TileLink } from "./styled";
 
-interface WrapperProps {
+interface LinkProps {
   linkTo?: string;
   children: React.ReactNode;
 }
 
 /**
- * The Wrapper component is used to conditionally wrap its children with a TileLink.
+ * The Link component is used to conditionally wrap its children with a TileLink.
  * If the 'linkTo' prop is provided, the children will be wrapped with a TileLink.
  * Otherwise, the children will be rendered as-is.
  *
  * @param linkTo - (Optional) A string representing the URL to navigate to when the TileLink is clicked.
- * @param children - The React child elements to be rendered within the Wrapper component.
+ * @param children - The React child elements to be rendered within the Link component.
  */
-const Link = ({ linkTo, children }: WrapperProps) => {
+const Link = ({ linkTo, children }: LinkProps) => {
   return linkTo ? <TileLink to={linkTo}>{children}</TileLink> : <>{children}</>;
 };
 
